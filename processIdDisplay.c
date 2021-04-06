3e9e9a1b670b9af771eb799baa66abba2e914785
AmeyaKulkarni2001 <71250891+AmeyaKulkarni2001@users.noreply.github.com>
2021-04-06 13:00:17 +0530

    Create processIdDisplay.c

---
+++ processIdDisplay.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+pid_t num_pid;
+
+pid_t num_pid;
+
+int main()
+{
+    num_pid=fork();
+    if(num_pid==0)
+    {
+        printf("This is the child process id %d\n",getpid());
+        
+    } if(num_pid>0) {
+        
+        printf("This is the parent process id %d\n",getpid());
+    }
+    return 0;
+    
+}
